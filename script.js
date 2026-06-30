ef982c0115883eddae9dee81894c22f74502bdb8
1444-aron <74068784+1444-aron@users.noreply.github.com>
2026-07-01 01:08:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+            const menu = document.getElementById("mobileMenu");
+            // Ha rajta van az 'open' osztály, leveszi (bezárja), ha nincs, rárakja (kinyitja)
+            menu.classList.toggle("open");
+        }
